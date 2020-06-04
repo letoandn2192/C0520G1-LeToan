41b93b52ccfeaa25fd798c7e29555f3f5eb5dbf5
letoandn2192 <talaquanlaogia123@gmail.com>
2020-06-04 19:32:25 +0700

    hello

--- Module_1/8.Cau_Truc_Dieu_Kien_1/Thuc_Hanh/[Prac]Prac_with_if_else.js
+++ Module_1/8.Cau_Truc_Dieu_Kien_1/Thuc_Hanh/[Prac]Prac_with_if_else.js
@@ -1,10 +1,10 @@
 function checkInfo() {
     let idCheck = document.getElementById('id').value;
     let passCheck = document.getElementById('password').value;
-    if(idCheck == 'Admin'){
-        if(passCheck == 'TheMaster'){
+    if(idCheck === 'Admin'){
+        if(passCheck === 'TheMaster'){
             document.getElementById('screen').innerHTML = 'Welcome !';
-        }else if(passCheck == ''){
+        }else if(passCheck == null){
             document.getElementById('screen').innerHTML = 'Canceled';
         }else{
             document.getElementById('screen').innerHTML = 'Wrong Password';
@@ -14,4 +14,5 @@ function checkInfo() {
     }else{
         document.getElementById('screen').innerHTML = "I don't know you";
     }
+    alert(typeof document.getElementById('id').value);
 }
