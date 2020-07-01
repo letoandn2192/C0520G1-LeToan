a5a7e1ebcb08b4c214fdb46ab89319241d2afef6
letoandn2192 <talaquanlaogia123@gmail.com>
2020-07-01 15:20:18 +0700

    hello

---
+++ Module_2/src/array_and_method_in_array/bai_tap/DemSoLanXuatHienKiTu.java
@@ -0,0 +1,20 @@
+package array_and_method_in_array.bai_tap;
+
+import java.util.Scanner;
+
+public class DemSoLanXuatHienKiTu {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int count = 0;
+        System.out.println("Nhập chuỗi bạn muốn kiểm tra: ");
+        String checkString = input.nextLine();
+        System.out.println("Nhập kí tự bạn muốn kiểm tra: ");
+        char character = input.next().charAt(0);
+        for(int i = 0; i < checkString.length(); i++){
+            if(character == checkString.charAt(i)){
+               count++;
+            }
+        }
+        System.out.println("Sô lần xuất hiện của kí tự "+ character + " là: " + count);
+    }
+}
